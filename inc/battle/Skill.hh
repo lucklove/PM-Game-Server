86985175e4f4ef3ca53b6a4065b4157d1299a864
Jos <gnu.crazier@gmail.com>
2015-12-09 18:15:32 +0800

    update skill

--- inc/battle/Skill.hh
+++ inc/battle/Skill.hh
@@ -17,17 +17,19 @@ struct Skill
     int rate_attr;          /**< 技能追加能力概率 */
     int attr;               /**< 技能追加能力属性 */
     int lvl_attr;           /**< 技能追加能力等级 */
+    int role_attr;          /**< 能力变化对象 */
     int rate_debuff;        /**< 技能追加异常概率 */
-    int debuff;             /**< 技能追加异常属性 */
-    int rate_cond;          /**< 技能追加特殊状态概率 */
-    int cond;               /**< 技能追加特殊状态 */
-    
+    int debuff;             /**< 技能追加异常类型 */
+    int role_debuff;        /**< 异常对象 */
+    int round;              /**< 异常回合数 */
+    int last_order;         /**< 技能后攻 */
+
     template <typename Archive>
     void serialize(Archive & ar)
     {
         ar(
-            id, name, type, skill_class, power, acc, is_melee, damage_hurt, weather_eff, 
-            rate_attr, attr, lvl_attr, rate_debuff, debuff, rate_cond, cond
+            id, name, type, skill_class, power, acc, is_melee, damage_hurt, weather_eff, rate_attr, 
+            attr, lvl_attr, role_attr, rate_debuff, debuff, role_debuff, round, last_order
         );
     }
 
@@ -57,7 +59,8 @@ namespace soci
                 v.get<int>("id"), v.get<std::string>("name"), v.get<int>("type"), v.get<int>("class"), 
                 v.get<int>("power"), v.get<int>("acc"), v.get<int>("is_melee"), v.get<int>("damage_hurt"), 
                 v.get<int>("weather_eff"), v.get<int>("rate_attr"), v.get<int>("attr"), v.get<int>("lvl_attr"), 
-                v.get<int>("rate_debuff"), v.get<int>("debuff"), v.get<int>("rate_cond"), v.get<int>("cond")
+                v.get<int>("role_attr"), v.get<int>("rate_debuff"), v.get<int>("debuff"), v.get<int>("role_debuff"),
+                v.get<int>("round"), v.get<int>("last_order") 
             };
         }
     };
